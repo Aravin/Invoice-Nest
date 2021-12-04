297c4014ce652de61397b955d16eb06b1d195e37
Aravin <aravin.it@gmail.com>
2021-12-04 19:39:51 +0530

    Added App Layout

---
+++ code/client/component/Layouts/Layout.tsx
@@ -0,0 +1,86 @@
+// import Navbar from './navbar'
+// import Footer from './footer'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import React, { useLayoutEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  UsergroupAddOutlined,
+  PieChartOutlined,
+  AppstoreOutlined,
+  SnippetsOutlined,
+  UserOutlined,
+  BookOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+export default function AppLayout({ children }: any) {
+
+  const [collapsed, seCollapse] = useState(false);
+  
+  useLayoutEffect(() => {
+
+  }, [collapsed])
+
+  return (
+    <Layout className="min-h-screen h-screen" >
+      <Sider collapsible collapsed={collapsed} onCollapse={() => seCollapse(!collapsed)}>
+        {
+          !collapsed &&
+          <div className="logo flex-1 text-center align-middle text-white text-lg font-bold pt-0.5">ResumeNest.com</div>
+        }
+        {
+          collapsed &&
+          <div className="logo flex-1 text-center align-middle text-white text-lg font-bold pt-0.5">RN</div>
+        }
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Dashboard
+          </Menu.Item>
+          <Menu.Item key="2" icon={<BookOutlined />}>
+            Invoices
+          </Menu.Item>
+          <Menu.Item key="3" icon={<SnippetsOutlined />}>
+            Estimates
+          </Menu.Item>
+          <Menu.Item key="4" icon={<UsergroupAddOutlined />}>
+            Customers
+          </Menu.Item>
+          <Menu.Item key="5" icon={<AppstoreOutlined />}>
+            Items
+          </Menu.Item>
+          <Menu.Item key="6" icon={<UserOutlined />}>
+            Sales Persons
+          </Menu.Item>
+          {/* <SubMenu key="sub1" icon={<UserOutlined />} title="Customers">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu> */}
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        {/* <Header className="site-layout-background" style={{ padding: 0 }} /> */}
+        <Content className="m-4">
+          <Breadcrumb className="">
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background p-4 my-4 mx-1 min-h-[360px]" >
+            {children}
+          </div>
+        </Content>
+        <Footer className="text-center">InvoiceNest.com ©2018 Created by Aravin with 💝</Footer>
+      </Layout>
+    </Layout>
+  )
+}
--- code/client/pages/_app.tsx
+++ code/client/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import AppLayout from '../component/Layouts/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <AppLayout>
+    <Component {...pageProps} />
+  </AppLayout>
 }
 
 export default MyApp
