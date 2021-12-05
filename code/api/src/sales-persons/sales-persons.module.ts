f7253f2169d89a67c42b830712b7a252f938380d
Aravin <aravin.it@gmail.com>
2021-12-05 22:16:50 +0530

    Added Proper import in SalesPerson Module

--- code/api/src/sales-persons/sales-persons.module.ts
+++ code/api/src/sales-persons/sales-persons.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SalesPersonsService } from './sales-persons.service';
 import { SalesPersonsController } from './sales-persons.controller';
-import { Organization } from 'src/organizations/entities/organization.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SalesPerson } from './entities/sales-person.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Organization])],
+  imports: [TypeOrmModule.forFeature([SalesPerson])],
   controllers: [SalesPersonsController],
   providers: [SalesPersonsService],
 })
