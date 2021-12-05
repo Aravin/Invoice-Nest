b4dd3318118d2f689f18de765070db575da0a6ac
Aravin <aravin.it@gmail.com>
2021-12-05 12:55:17 +0530

    Added invoices boilerplate CRUD

--- code/api/src/app.module.ts
+++ code/api/src/app.module.ts
@@ -6,9 +6,10 @@ import { TaxesModule } from './taxes/taxes.module';
 import { SalesPersonsModule } from './sales-persons/sales-persons.module';
 import { ItemsModule } from './items/items.module';
 import { CustomersModule } from './customers/customers.module';
+import { InvoicesModule } from './invoices/invoices.module';
 
 @Module({
-  imports: [TaxesModule, SalesPersonsModule, ItemsModule, CustomersModule],
+  imports: [TaxesModule, SalesPersonsModule, ItemsModule, CustomersModule, InvoicesModule],
   controllers: [AppController, TaxesController],
   providers: [AppService],
 })
