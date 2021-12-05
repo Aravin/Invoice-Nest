a922b6058ca094f23525f72ec728a010a0f574a0
Aravin <aravin.it@gmail.com>
2021-12-05 12:57:42 +0530

    Added organizations boilerplate CRUD

--- code/api/src/app.module.ts
+++ code/api/src/app.module.ts
@@ -9,9 +9,10 @@ import { CustomersModule } from './customers/customers.module';
 import { InvoicesModule } from './invoices/invoices.module';
 import { EstimatesModule } from './estimates/estimates.module';
 import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
-  imports: [TaxesModule, SalesPersonsModule, ItemsModule, CustomersModule, InvoicesModule, EstimatesModule, UsersModule],
+  imports: [TaxesModule, SalesPersonsModule, ItemsModule, CustomersModule, InvoicesModule, EstimatesModule, UsersModule, OrganizationsModule],
   controllers: [AppController, TaxesController],
   providers: [AppService],
 })
