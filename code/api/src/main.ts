85d6082fedf4524f2e5f1a4b92189a229e8e54a1
Aravin <aravin.it@gmail.com>
2022-04-27 20:12:29 +0530

    Added Support to Swagger

--- code/api/src/main.ts
+++ code/api/src/main.ts
@@ -1,16 +1,28 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Invoice Nest')
+    .setDescription('Invoice Nest API description')
+    .setVersion('1.0')
+    .addTag('invoices')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 
