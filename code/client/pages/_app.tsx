4d34d48068da02f453fe5caf080fd4b27d1fd8cb
Aravin <aravin.it@gmail.com>
2022-05-13 21:10:31 +0530

    Added progressbar

--- code/client/pages/_app.tsx
+++ code/client/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import AppLayout from '../component/Layouts/Layout';
+import NextNProgress from 'nextjs-progressbar';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <AppLayout>
-    <Component {...pageProps} />
-  </AppLayout>
+  return (
+    <>
+      <NextNProgress color='#09d61d' height={5} showOnShallow={true} />
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
+  )
 }
 
 export default MyApp
