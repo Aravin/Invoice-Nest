5eeef861aaed5bb96f1fb180463bc43d8fe92be9
Aravin <aravin.it@gmail.com>
2022-05-13 22:20:50 +0530

    Added ability to save customer

--- code/client/component/Customer/CreateCustomer.tsx
+++ code/client/component/Customer/CreateCustomer.tsx
@@ -2,12 +2,19 @@
 import { Form, Input, Button, Checkbox, Select, DatePicker, Space, Table, Col, Row, Tabs } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import TextArea from 'antd/lib/input/TextArea';
+import axios from 'axios';
 const { TabPane } = Tabs;
 
 export default function CreateCustomer() {
 
-  const onFinish = (values: any) => {
+  const onFinish = async (values: any) => {
     console.log('Success:', values);
+    try {
+      await axios.post(process.env.API_PATH + '/customers', values);
+    } catch (err) {
+      // show the error in UI
+      console.log(err);
+    }
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -19,56 +26,62 @@ export default function CreateCustomer() {
       <Form
         layout='horizontal'
         name="basic"
-        labelCol={{ sm: {span: 8}, lg: {span: 4} }}
-        wrapperCol={{ sm: {span: 12}, lg: {span: 8} }}
+        labelCol={{ sm: { span: 8 }, lg: { span: 4 } }}
+        wrapperCol={{ sm: { span: 12 }, lg: { span: 8 } }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
 
-        <Form.Item label="First Name">
+        <Form.Item label="First Name"
+          name='firstName'
+          required={true}
+          rules={[{ required: true, min: 3, max: 50, message: 'First name is required, min 3 and max 50 characters!' }]}>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Last Name">
+        <Form.Item label="Last Name"
+          name='lastName'
+          required={true}
+          rules={[{ required: true, min: 2, max: 50, message: 'Last name is required, min 2 and max 50 characters!' }]}>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Company Name">
+        <Form.Item label="Company Name" name='company'>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Email">
+        <Form.Item label="Email" name='email'>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Phone">
+        <Form.Item label="Phone" name='phone'>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Website">
+        <Form.Item label="Website" name='website'>
           <Input />
         </Form.Item>
 
-        <Form.Item label="PAN">
+        <Form.Item label="PAN" name='pan'>
           <Input />
         </Form.Item>
 
-        <Form.Item label="GST">
+        <Form.Item label="GST" name='gst'>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Address">
+        <Form.Item label="Address" name='address'>
           <TextArea rows={2} />
         </Form.Item>
 
-        <Form.Item label="Remarks">
+        <Form.Item label="Remarks" name='remarks'>
           <TextArea rows={2} />
         </Form.Item>
 
         <Row className='p-4 m-4 gap-4 justify-center'>
-          <Button type="primary" size="large">Save</Button>
+          <Button type="primary" size="large" htmlType="submit">Save</Button>
           <Button size="large">Cancel</Button>
         </Row>
       </Form>
--- code/client/next.config.js
+++ code/client/next.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_PATH: 'http://localhost:3000/v1',
+  },
 }
