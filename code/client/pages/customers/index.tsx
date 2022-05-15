9f4ac5bed00eb815ece9aace2bb06f69bdf6eed6
Aravin <aravin.it@gmail.com>
2022-05-15 10:43:39 +0530

    Added getServerSideProps

--- code/client/pages/customers/index.tsx
+++ code/client/pages/customers/index.tsx
@@ -1,9 +1,42 @@
-import { Col, PageHeader, Row } from 'antd'
-import type { NextPage } from 'next'
-import React, { } from 'react'
-import ListCustomer from '../../component/Customer/ListCustomer'
+import { PageHeader, Space, Table } from 'antd'
+import axios from 'axios';
+import type { GetServerSideProps } from 'next'
+import React from 'react'
 
-const CustomerPage: NextPage = () => {
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'firstName',
+    key: 'firstName',
+  },
+  {
+    title: 'Company',
+    dataIndex: 'company',
+    key: 'company',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Phone',
+    key: 'phone',
+    dataIndex: 'phone',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text: any, record: any) => (
+      <Space size="middle">
+        <a>Edit {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+export default function CustomerPage(props: any) {
   return (
     <div>
       <PageHeader
@@ -12,9 +45,17 @@ const CustomerPage: NextPage = () => {
         subTitle="List"
       />
 
-      <ListCustomer></ListCustomer>
+      <Table columns={columns} dataSource={props?.customers} className='m-2'/>
     </div>
   )
 }
 
-export default CustomerPage
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  const result = await axios.get(process.env.API_PATH + '/customers');
+
+  return {
+    props: {
+      customers: result.data,
+    },
+  }
+}
