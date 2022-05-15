69fec931d65398db80af4cc121186bd3cb3590b8
Aravin <aravin.it@gmail.com>
2022-05-15 17:03:34 +0530

    Changed reactStrictMode to false

--- code/client/next.config.js
+++ code/client/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   env: {
     API_PATH: 'http://localhost:3000/v1',
