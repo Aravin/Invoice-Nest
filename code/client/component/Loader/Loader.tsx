7c7b651d148d484d84d44ec26b6639545c21abd3
Aravin <aravin.it@gmail.com>
2022-05-15 19:00:28 +0530

    Added Loader to the app

---
+++ code/client/component/Loader/Loader.tsx
@@ -0,0 +1,30 @@
+export default function Loader() {
+  return (
+    <div className="flex h-96">
+      <div className="m-auto">
+        <div className="lds-roller">
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+        </div>
+        <div>
+          Loading...
+        </div>
+      </div>
+    </div>
+
+  );
+}
