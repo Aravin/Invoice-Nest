4b37bf5731c3efe6c50e4037c247c1d1da0015b8
Aravin <aravin.it@gmail.com>
2022-05-15 11:41:02 +0530

    Added Customer List Table

--- code/client/pages/customers/index.tsx
+++ code/client/pages/customers/index.tsx
@@ -1,18 +1,21 @@
-import { PageHeader, Space, Table } from 'antd'
+import { Button, PageHeader, Space, Table } from 'antd'
 import axios from 'axios';
 import type { GetServerSideProps } from 'next'
 import React from 'react'
+import { PlusOutlined } from '@ant-design/icons';
 
 const columns = [
   {
     title: 'Name',
     dataIndex: 'firstName',
     key: 'firstName',
+    fixed: 'left' as any,
   },
   {
     title: 'Company',
     dataIndex: 'company',
     key: 'company',
+    fixed: 'left' as any,
   },
   {
     title: 'Email',
@@ -25,8 +28,64 @@ const columns = [
     dataIndex: 'phone',
   },
   {
+    title: 'Website',
+    key: 'website',
+    dataIndex: 'website',
+  },
+  {
+    title: 'GST',
+    key: 'gst',
+    dataIndex: 'gst',
+  },
+  {
+    title: 'PAN',
+    key: 'pan',
+    dataIndex: 'pan',
+  },
+  {
+    title: 'Tax ID',
+    key: 'taxId',
+    dataIndex: 'taxId',
+  },
+  {
+    title: 'Address Line1',
+    key: 'addressLine1',
+    dataIndex: 'addressLine1',
+  },
+  {
+    title: 'Address Line2',
+    key: 'addressLine2',
+    dataIndex: 'addressLine2',
+  },
+  {
+    title: 'City',
+    key: 'city',
+    dataIndex: 'city',
+  },
+  {
+    title: 'State',
+    key: 'state',
+    dataIndex: 'state',
+  },
+  {
+    title: 'Country',
+    key: 'country',
+    dataIndex: 'country',
+  },
+  {
+    title: 'Zip',
+    key: 'zip',
+    dataIndex: 'zip',
+  },
+  {
+    title: 'Remarks',
+    key: 'remarks',
+    dataIndex: 'remarks',
+  },
+  {
     title: 'Action',
     key: 'action',
+    fixed: 'right' as any,
     render: (text: any, record: any) => (
       <Space size="middle">
         <a>Edit {record.name}</a>
@@ -43,9 +102,12 @@ export default function CustomerPage(props: any) {
         className="site-page-header"
         title="Customer"
         subTitle="List"
+        extra={[
+          <Button key="1" type="primary" icon={<PlusOutlined />}> Add Customer </Button>,
+        ]}
       />
 
-      <Table columns={columns} dataSource={props?.customers} className='m-2'/>
+      <Table columns={columns} dataSource={props?.customers} scroll={{ x: 3000 }} className='m-2'/>
     </div>
   )
 }
