972aa05a91039ccb3e7711104c11007c50f3b974
Aravin <aravin.it@gmail.com>
2022-05-15 19:00:50 +0530

    Improved UX with alert and proper messages

--- code/client/component/Customer/CustomerForm.tsx
+++ code/client/component/Customer/CustomerForm.tsx
@@ -1,29 +1,40 @@
-import { Form, Input, Button, Row } from 'antd';
+import { Form, Input, Button, Row, Alert } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import axios from 'axios';
 import NextRouter, { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Loader from '../Loader/Loader';
 
 export default function CustomerForm(props: any) {
 
   const router = useRouter();
   const action = router.query?.action;
   const customerId = router.query?.id;
-
+  const [isLoading, setLoader] = useState(false);
+  const [isSuccess, setSuccess] = useState(false);
+  const [isError, setError] = useState(false);
   const [form] = Form.useForm()
 
   useEffect(() => {
     (async () => {
       if (action === 'edit') {
-        const result = await axios.get(process.env.API_PATH + '/customers/' + customerId);
-        console.log(result.data);
-        form.setFieldsValue(result.data);
+        try {
+          setLoader(true);
+          const result = await axios.get(process.env.API_PATH + '/customers/' + customerId);
+          form.setFieldsValue(result.data);
+        } catch (err) {
+          console.error(err);
+          setError(true);
+        } finally {
+          setLoader(false);
+        }
       }
     })();
-  })
+  }, [])
 
   const onFinish = async (values: any) => {
-    console.log('Success:', values);
+    setLoader(true);
+
     try {
       if (action === 'edit') {
         await axios.patch(process.env.API_PATH + '/customers/' + customerId, values);
@@ -33,9 +44,12 @@ export default function CustomerForm(props: any) {
         // show error 
       }
       router.push('/customers');
+      setSuccess(true);
     } catch (err) {
-      // show the error in UI
       console.log(err);
+      setError(true);
+    } finally {
+      setLoader(false);
     }
   };
 
@@ -44,70 +58,82 @@ export default function CustomerForm(props: any) {
   };
 
   return (
-    <div className="">
-      <Form
-      form={form}
-        layout='horizontal'
-        name="basic"
-        labelCol={{ sm: { span: 8 }, lg: { span: 4 } }}
-        wrapperCol={{ sm: { span: 12 }, lg: { span: 8 } }}
-        initialValues={{}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
+    <div>
+      {
+        isSuccess && <Alert message="New Customer added successfully!" type="success" closable showIcon className='p-4' />
+      }
+      {
+        isError && <Alert message="Unable to add/update the new customer, please try again!" type="error" closable showIcon className='p-4' />
+      }
+      {
+        isLoading && <Loader />
+      }
+      {
+        !isLoading  &&
+        <Form
+          form={form}
+          layout='horizontal'
+          name="basic"
+          labelCol={{ sm: { span: 8 }, lg: { span: 4 } }}
+          wrapperCol={{ sm: { span: 12 }, lg: { span: 8 } }}
+          initialValues={{}}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
 
-        <Form.Item label="First Name"
-          name='firstName'
-          required={true}
-          rules={[{ required: true, min: 3, max: 50, message: 'First name is required, min 3 and max 50 characters!' }]}>
-          <Input />
-        </Form.Item>
+          <Form.Item label="First Name"
+            name='firstName'
+            required={true}
+            rules={[{ required: true, min: 3, max: 50, message: 'First name is required, min 3 and max 50 characters!' }]}>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Last Name"
-          name='lastName'
-          required={true}
-          rules={[{ required: true, min: 2, max: 50, message: 'Last name is required, min 2 and max 50 characters!' }]}>
-          <Input />
-        </Form.Item>
+          <Form.Item label="Last Name"
+            name='lastName'
+            required={true}
+            rules={[{ required: true, min: 2, max: 50, message: 'Last name is required, min 2 and max 50 characters!' }]}>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Company Name" name='company'>
-          <Input />
-        </Form.Item>
+          <Form.Item label="Company Name" name='company'>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Email" name='email'>
-          <Input />
-        </Form.Item>
+          <Form.Item label="Email" name='email'>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Phone" name='phone'>
-          <Input />
-        </Form.Item>
+          <Form.Item label="Phone" name='phone'>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Website" name='website'>
-          <Input />
-        </Form.Item>
+          <Form.Item label="Website" name='website'>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="PAN" name='pan'>
-          <Input />
-        </Form.Item>
+          <Form.Item label="PAN" name='pan'>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="GST" name='gst'>
-          <Input />
-        </Form.Item>
+          <Form.Item label="GST" name='gst'>
+            <Input />
+          </Form.Item>
 
-        <Form.Item label="Address" name='address'>
-          <TextArea rows={2} />
-        </Form.Item>
+          <Form.Item label="Address" name='address'>
+            <TextArea rows={2} />
+          </Form.Item>
 
-        <Form.Item label="Remarks" name='remarks'>
-          <TextArea rows={2} />
-        </Form.Item>
+          <Form.Item label="Remarks" name='remarks'>
+            <TextArea rows={2} />
+          </Form.Item>
 
-        <Row className='p-4 m-4 gap-4 justify-center'>
-          <Button type="primary" size="large" htmlType="submit">Save</Button>
-          <Button size="large" onClick={NextRouter.back}>Cancel</Button>
-        </Row>
-      </Form>
+          <Row className='p-4 m-4 gap-4 justify-center'>
+            <Button type="primary" size="large" htmlType="submit">Save</Button>
+            <Button size="large" onClick={NextRouter.back}>Cancel</Button>
+          </Row>
+        </Form>
+      }
     </div>
   )
 }
--- code/client/pages/customers/index.tsx
+++ code/client/pages/customers/index.tsx
@@ -1,20 +1,40 @@
-import { Button, PageHeader, Space, Table } from 'antd'
+import { Alert, Button, Modal, PageHeader, Space, Table } from 'antd'
 import axios from 'axios';
 import type { GetServerSideProps } from 'next'
-import React from 'react'
-import { EditOutlined, EditFilled, DeleteFilled } from '@ant-design/icons';
+import React, { useState } from 'react'
+import { EditOutlined, EditFilled, DeleteFilled, ExclamationCircleOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
 
 export default function CustomerPage(props: any) {
 
   const router = useRouter();
+  const [isSuccess, setSuccess] = useState(false);
+  const [isError, setError] = useState(false);
+  const { confirm } = Modal;
 
   const goToAddCustomer = () => router.push(`/customers/create?action=new`);
   const goToEditCustomer = (id: string) => router.push(`/customers/create?action=edit&id=${id}`);
+
   const deleteCustomer = async (id: string) => {
-    await axios.delete(process.env.API_PATH + '/customers/' + id);
-    router.replace('/customers');
-  };
+
+    confirm({
+      title: 'Do you want to delete this items?',
+      icon: <ExclamationCircleOutlined />,
+      async onOk() {
+        try {
+          await axios.delete(process.env.API_PATH + '/customers/' + id);
+          router.replace('/customers');
+          setSuccess(true);
+        } catch (err) {
+          console.error(err);
+          setError(true);
+        }
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
 
   const columns = [
     {
@@ -118,6 +138,13 @@ export default function CustomerPage(props: any) {
           <Button key="1" type="primary" icon={<EditOutlined />} onClick={goToAddCustomer}> Add Customer </Button>,
         ]}
       />
+
+      {
+        isSuccess && <Alert message="Customer deleted successfully!" type="success" closable showIcon className='p-4' />
+      }
+      {
+        isError && <Alert message="Unable to delete the new customer, please try again!" type="error" closable showIcon className='p-4' />
+      }
 
       <Table columns={columns} dataSource={props?.customers} scroll={{ x: 3000 }} className='m-2' />
     </div>
