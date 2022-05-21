7d4c80e3f685be4fa37fc976a1b6e34c8111ea5a
Aravin <aravin.it@gmail.com>
2022-05-22 00:42:08 +0530

    Changed .env orm file to .ts file

---
+++ code/api/orm.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const ormConfig: TypeOrmModuleOptions = {
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USR,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  logging: true,
+  autoLoadEntities: true,
+  synchronize: true,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+};
