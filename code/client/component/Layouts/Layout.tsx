a6dc534ccdeba35f040e65143ec9d0c86f8d03ff
Aravin <aravin.it@gmail.com>
2022-05-21 11:01:01 +0530

    Added handleClick to setting menu

--- code/client/component/Layouts/Layout.tsx
+++ code/client/component/Layouts/Layout.tsx
@@ -42,7 +42,7 @@ export default function AppLayout({ children }: any) {
   );
 
   const settingsMenu = (
-    <Menu>
+    <Menu onClick={handleClick}>
       <Menu.Item key='/organizations/create'>Organization</Menu.Item>
       <Menu.Item key='/users/create'>Users</Menu.Item>
       <Menu.Item key='/settings/create'>Settings</Menu.Item>
