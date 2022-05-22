ba27e4faf3ebfac4d35f06659246e9536468c479
Aravin <aravin.it@gmail.com>
2022-05-22 16:59:40 +0530

    Added nextjs-auth0

--- code/client/pages/_app.tsx
+++ code/client/pages/_app.tsx
@@ -2,15 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import AppLayout from '../component/Layouts/Layout';
 import NextNProgress from 'nextjs-progressbar';
-
+import { UserProvider, withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextNProgress color='#09d61d' height={5} showOnShallow={true} />
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
+      <UserProvider>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </UserProvider>
     </>
   )
 }
